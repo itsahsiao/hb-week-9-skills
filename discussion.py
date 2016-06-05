1751cc237d1b6cdde42672606fb74a681fa4c575
Ashley Hsiao <ahsiao@Ashleys-MacBook-Pro.local>
2016-06-04 17:53:59 -0700

    Completed Recursion questions

---
+++ discussion.py
@@ -0,0 +1,12 @@
+"""
+PART 1: DISCUSSION QUESTIONS
+
+RECURSION
+
+1.  Recursion is a function that calls itself. When it reaches the Base Case, it returns a value, but it needs to return previous functions and close them. This is like Russian nesting dolls that open but need to close each one when it goes back.
+
+2.  It is necessary to have a Base Case to know when recursion is done. If there is no Base Case, a Runtime Error will occur.
+
+
+
+"""
