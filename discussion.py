ca034fbc4055f79205698556a0e1443f7ae0ec4e
Ashley Hsiao <ahsiao@Ashleys-MacBook-Pro.local>
2016-06-04 18:23:27 -0700

    Completed second table for data structures

--- discussion.py
+++ discussion.py
@@ -20,17 +20,26 @@ GRAPHS
 PERFORMANCE OF DIFFERENT DATA STRUCTURES
 
 Data Structure                  Index   Search  Add-R   Add-L   Pop-L   Pop-R
-Python List (Array)             O(1)    O(n)    O(1)    O(n)    O(n)    O(1)         
+Python List (Array)             O(1)    O(n)    O(1)    O(n)    O(n)    O(1)
 Linked List                     O(n)    O(n)    O(1)    O(1)    O(1)    O(n)
-Doubly-Linked List              O(n)    O(n)    O(1)    O(1)    O(1)    O(1)         
+Doubly-Linked List              O(n)    O(n)    O(1)    O(1)    O(1)    O(1)
 Queue (as Array)                X       X       O(1)    X       O(n)    X
 Queue (as LL or DLL)            X       X       O(1)    X       O(1)    X
 Stack (as Array, LL, or DLL)    X       X       O(1)    X       X       O(1)**
-Deque (as DLL)                  X       X       O(1)    O(1)    O(1)    O(1)     
+Deque (as DLL)                  X       X       O(1)    O(1)    O(1)    O(1)
 
 
 **  Stack - Pop-R
     I originally put down O(n) due to Linked List being O(n) for Pop-R.
     But the trick for using LL is to keep the stack reversed, so you can push onto the head and pop off from the head, rather than the tail, and this way both push and pop can be O(1) operations for a singly-linked list.
+
+
+Data Structure          Get         Add     Delete  Iterate Memory
+Dictionary (Hash Map)   O(1)        O(1)    O(1)    O(n)    medium
+Set (Hash Map)          O(1)        O(1)    O(1)    O(n)    medium
+Binary Search Tree      O(log n)    O(n)    O(n)    O(1)    a little
+Tree                    O(n)        O(1)    O(1)    O(1)    a little
+
+
 
 """
