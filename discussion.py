52dc99adae9641fa7be39268c652297625af3ade
Ashley Hsiao <ahsiao@Ashleys-MacBook-Pro.local>
2016-06-04 18:14:43 -0700

    Completed first table for data structures

--- discussion.py
+++ discussion.py
@@ -7,6 +7,7 @@ RECURSION
 
 2.  It is necessary to have a Base Case to know when recursion is done. If there is no Base Case, a Runtime Error will occur.
 
+
 GRAPHS
 
 1.  A Graph is like a Tree, except it can contain loops ("cycles") and the relationships can be directed or non-directed.
@@ -15,5 +16,21 @@ GRAPHS
 
 3.  An example of something that would be good to model with a Graph is users who are friends with other users, like Facebook.
 
+
+PERFORMANCE OF DIFFERENT DATA STRUCTURES
+
+Data Structure                  Index   Search  Add-R   Add-L   Pop-L   Pop-R
+Python List (Array)             O(1)    O(n)    O(1)    O(n)    O(n)    O(1)         
+Linked List                     O(n)    O(n)    O(1)    O(1)    O(1)    O(n)
+Doubly-Linked List              O(n)    O(n)    O(1)    O(1)    O(1)    O(1)         
+Queue (as Array)                X       X       O(1)    X       O(n)    X
+Queue (as LL or DLL)            X       X       O(1)    X       O(1)    X
+Stack (as Array, LL, or DLL)    X       X       O(1)    X       X       O(1)**
+Deque (as DLL)                  X       X       O(1)    O(1)    O(1)    O(1)     
+
+
+**  Stack - Pop-R
+    I originally put down O(n) due to Linked List being O(n) for Pop-R.
+    But the trick for using LL is to keep the stack reversed, so you can push onto the head and pop off from the head, rather than the tail, and this way both push and pop can be O(1) operations for a singly-linked list.
 
 """
