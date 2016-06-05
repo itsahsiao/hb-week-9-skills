b3c2490cd21fbbfe812d858943594a8960b9453a
Ashley Hsiao <ahsiao@Ashleys-MacBook-Pro.local>
2016-06-04 17:57:30 -0700

    Completed questions for Graphs

--- discussion.py
+++ discussion.py
@@ -7,6 +7,13 @@ RECURSION
 
 2.  It is necessary to have a Base Case to know when recursion is done. If there is no Base Case, a Runtime Error will occur.
 
+GRAPHS
+
+1.  A Graph is like a Tree, except it can contain loops ("cycles") and the relationships can be directed or non-directed.
+
+2.  A Graph is different from a Tree because a Graph does not have hierarchy, but a Tree does.
+
+3.  An example of something that would be good to model with a Graph is users who are friends with other users, like Facebook.
 
 
 """
